889419d2370a0bf85770de4ad70c13a7b464a9d0
David Hui <dyth2@cam.ac.uk>
2017-08-12 00:12:40 +0100

    implemented overtaking

--- traffic.py
+++ traffic.py
@@ -7,15 +7,18 @@ distance = 16000
 
 class car:
     """a car has a position and speed"""
-    def __init__(self, initialX, speed):
+    def __init__(self, initialX, speed, patience):
         self.speed = speed
         self.speed2 = None
         self.position = [initialX*distance, 0]
+        self.patience = patience
+        self.count = 0
 
     def move(self, cars):
         # speed reflects the inherent speed the driver wants to travel at
         # a driver may be impeded by another, thus enforcing a slower speed2
         if self.speed2 != None:
+            self.count += 1
             if self.position[0] > distance:
                 self.position[0] = self.speed2
             else:
@@ -25,27 +28,35 @@ class car:
                 self.position[0] = self.speed
             else:
                 self.position[0] += self.speed
+        # patience represents how many steps the driver can tolerate at a slower
+        # speed. Once count reaches patience, the driver overtakes
+        if self.count == self.patience:
+            self.count = 0
+            self.speed2 = None
+            self.position[0] += 100
         # if there is a car of distance 10 away, adopt its speed
         # otherwise, use original speed
         for car in cars:
-            if car.position[0] != self.position[0]:
-                if ((car.position[0] - self.position[0] < 10.0)
-                        and (car.position[0] - self.position[0] > 0.0)):
-                    if car.position[1] == self.position[1]:
-                        if car.speed2 != None:
-                            self.speed2 = car.speed2
-                        else:
-                            self.speed2 = car.speed
-                        break
+            if ((car.position[0] != self.position[0])
+                and (car.position[0] - self.position[0] < 10.0)
+                and (car.position[0] - self.position[0] > 0.0)
+                and (car.position[1] == self.position[1])):
+                if car.speed2 != None:
+                    self.speed2 = car.speed2
                 else:
-                    self.speed2 = None
+                    self.speed2 = car.speed
+                    return
+            else:
+                self.speed2 = None
+                self.count = 0
 
             
 meanSpeed, stdev, samples = 50, 10, 100
 speeds = np.random.normal(meanSpeed, stdev, samples)
+patiences = np.random.poisson(2, samples)
 cars = []
 for i in range(samples):
-    cars.append(car(i/float(samples), speeds[i]))
+    cars.append(car(i/float(samples), speeds[i], patiences[i]))
 
 plt.ion()
 while True:
