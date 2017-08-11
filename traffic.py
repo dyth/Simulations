fa9202d2b896636fb8baeb50346a8a6a7343d520
David Hui <dyth2@cam.ac.uk>
2017-08-12 00:51:34 +0100

    yes, you do have to wait a long time for two buses

--- traffic.py
+++ traffic.py
@@ -3,18 +3,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-distance = 16000
+
+# global variables
+meanSpeed, stdev, samples, distance = 50, 10, 100, 16000
+
 
 class car:
     """a car has a position and speed"""
     def __init__(self, initialX, speed, patience):
-        self.speed = speed
         self.speed2 = None
-        self.position = [initialX*distance, 0]
+        self.position = [initialX, 0]
         self.patience = patience
         self.count = 0
+        if (initialX/100 == initialX/100.0):
+            self.colour = 'r'
+            self.speed = meanSpeed
+        else:
+            self.colour = 'b'
+            self.speed = speed
 
     def move(self, cars):
+        'move a car forwards'
         # speed reflects the inherent speed the driver wants to travel at
         # a driver may be impeded by another, thus enforcing a slower speed2
         if self.speed2 != None:
@@ -50,21 +59,20 @@ class car:
                 self.speed2 = None
                 self.count = 0
 
-            
-meanSpeed, stdev, samples = 50, 10, 100
+
+# initialise list of cars -- depends on speeds and patiences
 speeds = np.random.normal(meanSpeed, stdev, samples)
-patiences = np.random.poisson(2, samples)
-cars = []
-for i in range(samples):
-    cars.append(car(i/float(samples), speeds[i], patiences[i]))
+patience = np.random.poisson(2, samples)
+cars = [car(distance*i/samples, speeds[i], patience[i]) for i in range(samples)]
 
 plt.ion()
 while True:
-    # plot car
+    # plot all vehicles, if car then blue else red for bus
     plt.clf()
     xs = [car.position[0] for car in cars]
     ys = [car.position[1] for car in cars]
-    plt.plot(xs, ys, 'o')
+    colour = [car.colour for car in cars]
+    plt.scatter(xs, ys, c=colour)
     plt.axis([0, distance, -10, 10])
     plt.pause(0.1)
     # move all cars
